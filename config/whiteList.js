a42c636f30fac417f9fab3215c1059151d3faf5f
Jeeva-code25 <jeevananthankumar@gmail.com>
2025-02-18 11:43:21 +0530

    Update whiteList.js

--- config/whiteList.js
+++ config/whiteList.js
@@ -1,3 +1,3 @@
-const whiteList = ['http://localhost:3000',  'http://localhost:5173']
+const whiteList = ['https://blogs-jeeva-code25.netlify.app/', 'http://localhost:3000',  'http://localhost:5173']
 
 module.exports = whiteList
